d3d62fc41081802280a186fa3395d7d45a2b1cc6
Virgiawan Listiyandi <listiyandiv@gmail.com>
2025-07-09 16:54:17 +0700

    lesson 2

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 document.addEventListener('alpine:init', () => {
-  Alpine.data('counter', () => ({
-    count: 0,
-    name: 'Virgiawan',
-    logCount() {
-      console.log(this.count);
-    },
+  Alpine.data('signupForm', () => ({
+    username: '',
+    password: '',
+    passwordConfirm: '',
+    belt: '',
+    bio: '',
+    newsletter: true,
   }));
 });
