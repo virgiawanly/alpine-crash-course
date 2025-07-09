be50a66c5d4bd3b6acf0e8f1e4fc5ce2d0337933
Virgiawan Listiyandi <listiyandiv@gmail.com>
2025-07-09 18:58:08 +0700

    lesson 6

--- index.js
+++ index.js
@@ -7,9 +7,39 @@ document.addEventListener('alpine:init', () => {
     bio: '',
     newsletter: true,
     showPass: false,
+    errors: {},
 
-    submitForm() {
-      console.log(this.username, this.password, this.passwordConfirm, this.belt, this.bio, this.newsletter);
+    validateForm() {
+      this.errors = {};
+
+      if (this.username.length < 3) {
+        this.errors.username = 'Username must be at least 3 characters.';
+      }
+
+      if (this.password.length < 6) {
+        this.errors.password = 'Password must be at least 6 characters.';
+      }
+
+      if (this.password !== this.passwordConfirm) {
+        this.errors.passwordConfirm = 'Passwords do not match.';
+      }
+
+      if (!this.belt) {
+        this.errors.belt = 'Please select a belt color.';
+      }
+
+      if (this.bio.length < 10) {
+        this.errors.bio = 'Bio must be at least 10 characters.';
+      }
+    },
+
+    submitForm($event) {
+      this.validateForm();
+
+      if (!Object.keys(this.errors).length) {
+        console.log(this.username, this.password, this.passwordConfirm, this.belt, this.bio, this.newsletter);
+        $event.target.reset();
+      }
     },
   }));
 });
